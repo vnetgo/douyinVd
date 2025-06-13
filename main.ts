e6365e4af47a3e0cd6d19dcc03fb632529ccf0fb
coderpwh <plmoknpwh@163.com>
2025-06-13 20:01:10 +0800

    log input url

--- main.ts
+++ main.ts
@@ -4,7 +4,9 @@ Deno.serve(async (req) => {
 
   const url = new URL(req.url);
   if (url.searchParams.has("url")) {
-    const videoUrl = await getVideoUrl(url.searchParams.get("url")!);
+    const inputUrl = url.searchParams.get("url")!;
+    console.log("inputUrl:", inputUrl);
+    const videoUrl = await getVideoUrl(inputUrl);
     return new Response(videoUrl);
   } else {
     return new Response("请提供url参数");
