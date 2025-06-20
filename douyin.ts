4171bb116381fb5d217a4382be084d4f7d2df073
coderpwh <plmoknpwh@163.com>
2025-06-20 21:39:01 +0800

    支持图文链接解析

--- douyin.ts
+++ douyin.ts
@@ -1,3 +1,5 @@
+import {string} from "npm:zod@3.24.2";
+
 const pattern = /"video":{"play_addr":{"uri":"([a-z0-9]+)"/;
 const cVUrl =
   "https://www.iesdouyin.com/aweme/v1/play/?video_id=%s&ratio=1080p&line=0";
@@ -59,12 +61,68 @@ async function doGet(url: string): Promise<Response> {
   return resp;
 }
 
+async function parseImgList(body: string):Promise<string[]> {
+  const content = body.replace(/\\u002F/g, "/").replace(/\//g, '/')
+  // console.log(content)
+  const reg = /{"uri":"[^\s"]+","url_list":\["(https:\/\/p\d{1,2}-sign.douyinpic.com\/.*?)"/g;
+  const urlRet = /"uri":"([^\s"]+)","url_list":/g
+
+  let imgMatch;
+  const firstUrls: string[] = [];
+  while ((imgMatch = reg.exec(content)) !== null) {
+    firstUrls.push(imgMatch[1]);
+  }
+  // console.log('firstUrls.length:',firstUrls.length)
+// console.log(firstUrls)
+
+  let urlMatch;
+  const urlList: string[] = [];
+  while ((urlMatch = urlRet.exec(content)) !== null) {
+    urlList.push(urlMatch[1])
+  }
+  const urlSet = new Set(urlList)
+  // console.log('urlSet.size:',urlSet.size)
+  const rList = []
+
+  for (let urlSetKey of urlSet) {
+    // console.log('urlSetKey:',urlSetKey)
+
+    let t = firstUrls.find((item) => {
+      return item.includes(urlSetKey);
+    });
+    if (t) {
+      rList.push(t)
+    }
+  }
+
+//打印rList结果
+// console.log(rList)
+// console.log(rList.length);
+
+// 过滤掉包含 /obj/ 的链接
+  const filteredRList = rList.filter(url => !url.includes("/obj/"));
+
+// 输出过滤后的结果
+//   console.log(filteredRList);
+  console.log('filteredRList.length:',filteredRList.length)
+  return filteredRList;
+}
+
 async function getVideoInfo(url: string): Promise<DouyinVideoInfo> {
+  let type = "video";
+  let img_list: string[] = [];
+  let video_url = "";
   const resp = await doGet(url);
   const body = await resp.text();
   const match = pattern.exec(body);
-  if (!match || !match[1]) throw new Error("Video ID not found in URL");
-  const video_url = cVUrl.replace("%s", match[1]);
+  if (!match || !match[1]) {
+    type = "img";
+  }
+  if (type == "video") {
+    video_url = cVUrl.replace("%s", match![1]);
+  } else {
+    img_list = await parseImgList(body)
+  }
   const auMatch = body.match(regex);
   const ctMatch = body.match(ctRegex);
   const descMatch = body.match(descRegex);
@@ -90,8 +148,8 @@ async function getVideoInfo(url: string): Promise<DouyinVideoInfo> {
       desc: null,
       create_time: null,
       video_url: video_url,
-      type: "video",
-      image_url_list: null
+      type: type,
+      image_url_list: img_list,
     };
 
     if (auMatch) {
@@ -131,5 +189,12 @@ async function getVideoUrl(url: string): Promise<string> {
 // const url = "https://v.douyin.com/JyCk5gy";
 // const downloadUrl = await getVideoInfo(url);
 // console.log(downloadUrl);
+// https://v.douyin.com/nA55ZPqlGOc/
+// https://v.douyin.com/S1Z20qK0spM/
+const url = "https://v.douyin.com/JyCk5gy"
+const r = await getVideoInfo(url);
+console.log(r);
+
+
 
 export { getVideoUrl,getVideoInfo };
